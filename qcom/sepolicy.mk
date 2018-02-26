926a5a3fd17939ecf4e167125fa92b9fa7d4b993
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-02-26 20:28:40 +0100

    Revert "sepolicy: Policy for recovery"
    Revert "sepolicy: Isolate qcom sysfs_graphics policy for recovery"
    
    This reverts commit c4c96f6145838adb949b204ea7e3828ec2eb72a7.
    
    Revert "recovery: Clean up recovery SELinux policy"
    
    This reverts commit 2c9cfd4cb815bba6cdb6418e5006069cb8f26d85.
    
    Revert "common: Add sf_lcd_density_prop type and labelled props"
    
    This reverts commit d54feea5d968f51d66421605e178be1f43015dd5.
    
    Revert "sepolicy: Initial policy for recovery"
    
    This reverts commit 5e84bfc96ec99eb17be5fe09e9034e83590bf1a6.
    
    Change-Id: I7ebede8ee13c7a81e7954cc243b94c108a4dafad

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,9 +3,6 @@
 # that inherit from AICP
 #
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/aicp/sepolicy/qcom/private
-
 BOARD_SEPOLICY_DIRS += \
     device/aicp/sepolicy/qcom/common \
     device/aicp/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
