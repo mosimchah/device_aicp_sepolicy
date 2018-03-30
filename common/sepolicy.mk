fb63bf645fee515463baf2f2b65b1db4f3951f41
mosimchah <mosimchah@gmail.com>
2018-03-30 13:45:53 -0400

    diff
    Change-Id: Ic229f9f7fbe74851ebdb6519ea0e4371d1d5b8d5

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,13 +1,13 @@
 #
 # This policy configuration will be used by all products that
-# inherit from AICP
+# inherit from Lineage
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/aicp/sepolicy/common/public
+    device/lineage/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/aicp/sepolicy/common/private
+    device/lineage/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/aicp/sepolicy/common/vendor
+    device/lineage/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,8 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from AICP
+# that inherit from Lineage
 #
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lineage/sepolicy/qcom/private
+
 BOARD_SEPOLICY_DIRS += \
-    device/aicp/sepolicy/qcom/common \
-    device/aicp/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/lineage/sepolicy/qcom/common \
+    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
